347940a4e7b0349180aff1054f16280e7d54393e
nikhilleo <ramrakhyani94@gmail.com>
2021-03-07 22:18:18 +0530

    All CODE

---
+++ Firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAH1hOeUEYNPGL_ISFSOMM046utfyLKD6g",
+  authDomain: "netflix-backend-1008.firebaseapp.com",
+  projectId: "netflix-backend-1008",
+  storageBucket: "netflix-backend-1008.appspot.com",
+  messagingSenderId: "619620997647",
+  appId: "1:619620997647:web:dea8ed6e5b08156bd7e17a",
+  measurementId: "G-2MR0X82B2X"
+};
