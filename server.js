680e4348b432008339080d33944d221748c598b0
nikhilleo <ramrakhyani94@gmail.com>
2021-03-13 22:54:04 +0530

    Horror Updates At 1:50 AM

--- server.js
+++ server.js
@@ -73,6 +73,14 @@ cron.schedule("0 10 20 * * *", () => {
     })
 })
 
+cron.schedule("0 10 20 * * *", () => {
+    Axios.get(`https://netflix-clone-backend-1008.herokuapp.com/horror/updateHorror/`).then((res) => {
+        console.log(res.data)
+        console.log(Date(Date.now().toLocaleString()))
+    }).catch((err) => {
+        console.log(err.response.data);
+    })
+})
 // setInterval(() => {
 //     Axios.get("http://localhost:8080/").then((res) => {
 //         console.log(res.data)
