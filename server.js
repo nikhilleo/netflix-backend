81771ef13ca346938a950dcc4131adf3cd49a81f
nikhilleo <ramrakhyani94@gmail.com>
2021-03-13 22:52:22 +0530

    Comedy Updates At 1:40 AM

--- server.js
+++ server.js
@@ -64,6 +64,15 @@ cron.schedule("0 10 20 * * *", () => {
     })
 })
 
+cron.schedule("0 10 20 * * *", () => {
+    Axios.get(`https://netflix-clone-backend-1008.herokuapp.com/comedy/updateComedy/`).then((res) => {
+        console.log(res.data)
+        console.log(Date(Date.now().toLocaleString()))
+    }).catch((err) => {
+        console.log(err.response.data);
+    })
+})
+
 // setInterval(() => {
 //     Axios.get("http://localhost:8080/").then((res) => {
 //         console.log(res.data)
