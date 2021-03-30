797c07eacc22dbffa345bb796690c65a97cc33b8
nikhilleo <ramrakhyani94@gmail.com>
2021-03-30 22:27:48 +0530

    Routes Updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,18 @@
 const passport = require('passport');
 const router = require('express').Router();
 
+router.get('/checkLogged', (req, res) => {
+    console.log(req.user);
+    if(req.user){
+        res.send(req.user)
+        res.status(401).send("Not Authorized");
+    }
+    else
+    {
+        res.send(req.user)
+    }
+});
+
 // Google Auth
 router.get('/google', passport.authenticate("google", {
     scope: ['profile', 'email']
@@ -10,7 +22,7 @@ router.get('/google', passport.authenticate("google", {
 router.get('/google/callback', passport.authenticate("google", {
     failureRedirect: "/",
 }), (req, res) => {
-    res.redirect("/")
+    res.redirect("http://localhost:3000/select_page")
 });
 
 // Logout
